47680d631580cd8c4970bfe1883632b1c661291f
Jinwook Kim <jininja7@gmail.com>
2020-05-23 17:06:58 -0700

    add a new request method to accept either username/id

--- src/Duolingo.js
+++ src/Duolingo.js
@@ -1,8 +1,23 @@
 const axios = require('axios');
+const baseUrl = "https://www.duolingo.com/2017-06-30/users";
+const legacyAchievementDisplayNames = require("./Constants");
 
 class Duolingo {
-    constructor() {
+    constructor({username = '', id = ''}) {
+        this.setRequestUrl(username, id);
     }
+
+    setRequestUrl = (username, id) => {
+        if (username.length > 0) {
+            this.requestUrl = `${baseUrl}?username=${username}`;
+        }
+        else if (id.length > 0) {
+            this.requestUrl = `${baseUrl}/${id}`;
+        }
+        else {
+            throw new Error("Please provide a Username or an ID.");
+        }
+    };
 }
 
 module.exports = Duolingo;

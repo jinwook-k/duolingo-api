622d579267787172a3a2f6d7d74c8039514b50c6
Jinwook Kim <jininja7@gmail.com>
2020-05-23 17:06:03 -0700

    add translation to display name for legacy achievements

---
+++ src/Constants.js
@@ -0,0 +1,10 @@
+export const legacyAchievementDisplayNames = {
+    streak: 'Wildfire',
+    completion: 'Champion',
+    spending: 'Big Spender',
+    items: 'Outfits',
+    time: 'Overtime',
+    social: 'Friendly',
+    xp: 'Overachiever',
+    perfect: 'Sharpshooter'
+};
